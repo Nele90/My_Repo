cba57e770069e4ee96e25e622a9b1f511675e72e
Nele90 <65944528+Nele90@users.noreply.github.com>
2020-07-01 10:06:18 +0200

    Create Not_working.sql

---
+++ Scripts/Not_working.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset Bob_Dev:1
+Cressaate VIeEW Liweq_Test
+AS
+SfELECT * 
+FROM sysv .databases
+
+
+--rollback ALTER View Liq_Test AS SELECT database_id  FROM sys.databases
